a7f7a0b8f645f498847b233028934a2e25c662a0
monishkumar1999 <monishkumarapj@gmail.com>
2025-01-26 12:43:04 +0530

    react native

--- component/productDeatilsScreen.jsx
+++ component/productDeatilsScreen.jsx
@@ -1,1 +1,17 @@
-ref
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import product from "../constants/productList";
+import product from "../constants/productList";
+
+const productDeatilsScreen = () => {
+
+   const product=product[0]
+   console.log(product)
+  return <View>
+
+  </View>;
+};
+
+export default productDeatilsScreen;
+
+const styles = StyleSheet.create({});
