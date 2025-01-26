cbcd5877e089ded028aac236c8db410635005325
monishkumar1999 <monishkumarapj@gmail.com>
2025-01-26 20:09:28 +0530

    product  detail screen

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { View, Image, ScrollView } from 'react-native';
 import ProductScreen from '../../component/ProductScreen'; // Import the ProductScreen component
-
+import ProductDetailsScreen from '../../component/productDeatilsScreen';
 const Login = () => {
   return (
-    <View className="flex-1 p-4"> 
+    <View className=""> 
       {/* Display the ProductScreen component */}
 
     
-      <ProductScreen /> 
-     
+      {/* <ProductScreen />  */}
+      <ProductDetailsScreen /> 
       {/* Display an image with proper styling */}
-      <Image 
-        source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike3.png" }} 
-        className="w-96 h-96" // Tailwind styling
-      />
+     
    
     </View>
   );
--- component/productDeatilsScreen.jsx
+++ component/productDeatilsScreen.jsx
@@ -1,17 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { View, Image, FlatList, useWindowDimensions, Text, ScrollView } from "react-native";
 import product from "../constants/productList";
-import product from "../constants/productList";
 
-const productDeatilsScreen = () => {
+const ProductDetailsScreen = () => {
+  const productDetails = product[0];
+  const { width } = useWindowDimensions(); // Get the screen dimensions
 
-   const product=product[0]
-   console.log(product)
-  return <View>
+  return (
+    <View className=" bg-white">
+      {/* Product Image Carousel */}
+      <FlatList
+        data={productDetails.images}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        className=" p-16 border border-red-400"
+        renderItem={({ item }) => (
+          <Image
+            source={{ uri: item }}
+          
+            className="rounded-lg w-screen h-96"
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
 
-  </View>;
+      {/* Product Details */}
+      <ScrollView>
+      <View className="p-4">
+     
+        <Text className="font-bold text-3xl mb-2">{productDetails.name}</Text>
+        <Text className="font-bold text-2xl text-gray-700 mb-4">$ {productDetails.price}</Text>
+      
+        <Text className="text-base text-gray-600">{productDetails.description}</Text>
+        <Text className="text-base text-gray-600">{productDetails.description}</Text>
+        <Text className="text-base text-gray-600">{productDetails.description}</Text>
+        <Text className="text-base text-gray-600">{productDetails.description}</Text>
+        
+      </View>
+      </ScrollView>
+    </View>
+  );
 };
 
-export default productDeatilsScreen;
-
-const styles = StyleSheet.create({});
+export default ProductDetailsScreen;
