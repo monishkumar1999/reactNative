a9a984d943c9c430330ba7bff4cde3a3f5bffcc7
monishkumar1999 <monishkumarapj@gmail.com>
2025-01-29 16:56:39 +0530

    add cart section

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { View, Image, ScrollView } from 'react-native';
 import ProductScreen from '../../component/ProductScreen'; // Import the ProductScreen component
 import ProductDetailsScreen from '../../component/productDeatilsScreen';
+import ShoppingCart from '../../component/ShoppingCart';
 const Login = () => {
   return (
     <View className="flex-1"> 
       {/* Display the ProductScreen component */}
 
-    
+    <ShoppingCart/>
       {/* <ProductScreen />  */}
       <ProductDetailsScreen /> 
       {/* Display an image with proper styling */}
