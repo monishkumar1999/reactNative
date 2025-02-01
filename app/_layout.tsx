76e35e671e424c97fc15672b5c178c77820c4708
monishkumar1999 <monishkumarapj@gmail.com>
2025-02-02 00:03:52 +0530

    day 1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import "../global.css";
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack >
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text className='font-bold'>Hello, World!</Text>
+    </View>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Redirect } from 'expo-router';
 import React from 'react';
+import { Text, View } from 'react-native';
 
 const Index = () => {
   // Redirects the user to the `/home` route
-  return <Redirect href="/login" />;
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Hsello, Word!</Text>
+      </View>
+    );
 };
 
 export default Index;
